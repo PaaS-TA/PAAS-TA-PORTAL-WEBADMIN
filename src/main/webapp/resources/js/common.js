2844f07761e44012aad0d2873faa3bc62de64248
CissC2 <d3g3nyo@gmail.com>
2018-03-20 11:25:04 +0900

    Http Method Argument 두번쨰 인수로 추가 (메소드 오버로딩)

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -80,6 +80,50 @@ var procCallAjax = function(reqUrl, param, callback) {
     });
 };
 
+// CALL AJAX  reqMethod PUT, DELETE 추가
+var procCallAjax = function(reqUrl, reqMethod, param, callback) {
+    var reqData = "";
+    //var reqMethod = "POST";
+
+    if (param != null) {
+        reqData = JSON.stringify(param);
+
+    } else {
+        reqData = JSON.stringify({});
+    //    reqMethod = "GET";
+    }
+
+    $.ajax({
+        url: reqUrl,
+        method: reqMethod,
+        data: reqData,
+        dataType: 'json',
+        contentType: "application/json",
+        success: function(data) {
+            if (data) {
+                callback(data, param);
+            } else {
+                var resData = {RESULT : RESULT_STATUS_SUCCESS,
+                    RESULT_MESSAGE : RESULT_STATUS_SUCCESS_MESSAGE};
+
+                callback(resData, param);
+            }
+        },
+        error: function(xhr, status, error) {
+            var resData = {RESULT : RESULT_STATUS_FAIL,
+                RESULT_MESSAGE : JSON.parse(xhr.responseText).message};
+
+            callback(resData, param);
+
+            console.log("ERROR :: error :: ", error);
+            procAlert("danger", JSON.parse(xhr.responseText).message);
+        },
+        complete : function(data) {
+            console.log("COMPLETE :: data :: ", data);
+        }
+    });
+};
+
 
 // ALARM
 var timeoutHandler = null;
@@ -234,8 +278,10 @@ var objToJson = function(formData) {
 
 // MOVE PAGE
 var procMovePage = function(pageUrl) {
-    if (pageUrl == null || pageUrl.length < 1) return false;
-
+    if (pageUrl == null || pageUrl.length < 1){
+        return false;
+    }
+    console.log(pageUrl);
     location.href = pageUrl;
 };
 
