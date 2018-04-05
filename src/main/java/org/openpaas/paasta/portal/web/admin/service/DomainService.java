124a150d806e1c8a9ecaa909c436e108197f9026
swmoon <swmoon@bluedigm.com>
2018-04-05 20:39:52 +0900

    PortalWebAdmin - 카탈로그 관리페이지 - 수정/삭제/추가 기능 구현 - 파일업로드 기능 및 섬네일 기능 안됨

--- src/main/java/org/openpaas/paasta/portal/web/admin/service/DomainService.java
+++ src/main/java/org/openpaas/paasta/portal/web/admin/service/DomainService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 /**
  * Created by MIN on 2018-04-05.
  */
+@Service
 public class DomainService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DomainController.class);
     private final String V2_URL = "/v2";
