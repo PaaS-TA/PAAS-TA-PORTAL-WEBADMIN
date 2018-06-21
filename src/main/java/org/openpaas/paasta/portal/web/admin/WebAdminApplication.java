4c1f22e74929a92f9565a4fd42288e4991b6defb
swmoon <swmoon@bluedigm.com>
2018-06-21 16:54:40 +0900

    불필요 주석 제거

--- src/main/java/org/openpaas/paasta/portal/web/admin/WebAdminApplication.java
+++ src/main/java/org/openpaas/paasta/portal/web/admin/WebAdminApplication.java
@@ -7,23 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * Use  web-server.
- *
- * @author Paul Chapman
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class WebAdminApplication extends WebMvcConfigurerAdapter {
-
-	/**
-	 * Run the application using Spring Boot and an embedded servlet engine.
-	 *
-	 * @param args
-	 *            Program arguments - ignored.
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(WebAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminApplication.class, args);
+    }
 
 }
