9e04b24a28d33ed3641b87e41f22d18463ab3381
박철한 <parkcheolhan@bluedigm.com>
2018-10-24 13:07:12 +0900

    빌드 오류 수정

--- src/main/java/org/openpaas/paasta/portal/web/admin/service/SecurityGroupService.java
+++ src/main/java/org/openpaas/paasta/portal/web/admin/service/SecurityGroupService.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
-import sun.plugin2.message.GetAppletMessage;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
