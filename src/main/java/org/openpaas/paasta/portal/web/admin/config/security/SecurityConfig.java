f876724c52454cea55c0a790cd8e89af8c4df8be
swmoon <swmoon@bluedigm.com>
2019-05-17 10:33:11 +0900

    / 접속 오류 수정

--- src/main/java/org/openpaas/paasta/portal/web/admin/config/security/SecurityConfig.java
+++ src/main/java/org/openpaas/paasta/portal/web/admin/config/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/index").permitAll()
